3b2d81ff03224d5b72cc886c3bef1d3b7c72695a
Sepehr Raissian <sepehrtheraiss@gmail.com>
2024-08-02 12:02:18 -0700

    update

--- phaser_framework/index.js
+++ phaser_framework/index.js
@@ -13,10 +13,17 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+  this.load.image('background', 'img/breakout_bg.png');
+  this.load.image('paddle', 'img/paddle.png');
+  this.load.image('brick', 'img/brick.png');
+  this.load.spritesheet('ball', 'img/wobble.png', 20, 20);
+  this.load.spritesheet('button', 'img/button.png', 120, 40);
 }
 
 function create ()
 {
+  this.add.image(400, 300, 'background');
+  this.add.image(400, 300, '');
 }
 
 function update ()
